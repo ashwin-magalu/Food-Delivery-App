4e786a93bd95dafe24462636e76e8e7f1ebca599
ashwin-magalu <ashwin.magalu@gmail.com>
2021-01-05 13:00:42 +0530

    first commit

---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "AIzaSyBuL6R90KfZ1q-XdWPZf4Md_D1E";
+
+export default GOOGLE_API_KEY;
---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+/* To use custom fonts in our project */
+
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ["./assets/fonts/"],
+};
